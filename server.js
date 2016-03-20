5ab2bb8f9c8b223520b68cff78c1de73e4f85ef6
Richard van Berkum <z4nshin@gmail.com>
2016-03-21 00:42:01 +0100

    init

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+ 
+// must specify options hash even if no options provided!
+var phpExpress = require('php-express')({
+ 
+  // assumes php is in your PATH
+  binPath: 'php'
+});
+ 
+// set view engine to php-express
+app.set('views', './src');
+app.engine('php', phpExpress.engine);
+app.set('view engine', 'php');
+ 
+// routing all .php file to php-express
+app.all(/.+\.php$/, phpExpress.router);
+app.use(express.static('src'));
+
+/*
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('PHPExpress app listening at http://%s:%s', host, port);
+});
+*/
+module.exports = app;
+
